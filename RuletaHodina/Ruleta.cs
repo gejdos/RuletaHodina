bc9a2c257efda801eafdd5cc03821ab4ab62be31
Jakub Gejdos <gejdosjakub@gmail.com>
2017-12-15 20:24:02 +0100

    commit

--- RuletaHodina/Ruleta.cs
+++ RuletaHodina/Ruleta.cs
@@ -11,8 +11,17 @@ namespace RuletaHodina
         public Hrac Hrac;
         public HraciStol HraciStol = new HraciStol();
         
+        public void Zahraj(Stavka stavka, Hrac hrac)
+        {
+            // vytoci cislo
+            //HraciePole vytoceneHp = VytocCislo()
 
+            //porovna ci vyhral alebo nie a upravi hracov bank
+            //hrac.Bank += VratVyhru(stavka, vytoceneHp);
+            
 
+            //stavku zaznamena do historie
+        }
 
     }
 }
