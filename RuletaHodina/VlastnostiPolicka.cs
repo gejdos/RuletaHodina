58e9e0c0a43fd9a9903e6da21633c3d02efaad9b
Jakub Gejdos <gejdosjakub@gmail.com>
2017-12-15 17:39:59 +0100

    commit

---
+++ RuletaHodina/VlastnostiPolicka.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuletaHodina
+{
+    enum Farba
+    {
+        zelena,
+        cierna,
+        cervena
+    }
+
+    enum Parnost
+    {
+        ziadna,
+        parna,
+        neparna
+    }
+
+    enum Rad
+    {
+        prvy,
+        druhy,
+        treti
+    }
+
+    enum Rozsah
+    {
+        R1_12,
+        R13_24,
+        R25_36
+    }
+      
+}
