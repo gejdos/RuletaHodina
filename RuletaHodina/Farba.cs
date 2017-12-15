42d5fa5bab21c6072be3bd689799b133178dabc7
Jakub Gejdos <gejdosjakub@gmail.com>
2017-12-15 17:29:28 +0100

    commit

--- RuletaHodina/Farba.cs
+++ RuletaHodina/Farba.cs
@@ -6,12 +6,10 @@ using System.Threading.Tasks;
 
 namespace RuletaHodina
 {
-    class Program
+    enum Farba
     {
-        static void Main(string[] args)
-        {
-
-
-        }
+        zelena,
+        cierna,
+        cervena
     }
 }
